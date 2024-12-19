f0cce1f627a83931a3daf46eda768bd4aa612094
Zaranasirova <zerifenesirova784@gmail.com>
2024-12-19 22:05:52 +0400

    Complete header section and partially finish home section layout

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const Contact_data = [
+  {
+    type: "phone:",
+    value: "+994 50 331 99 74",
+  },
+  {
+    type: "email:",
+    value: "Zerifenesirova784@gmail.com",
+  },
+  {
+    type: "address:",
+    value: "Bakı ş, Binəqədi rayonu",
+  },
+];
