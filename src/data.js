edb44f4e27707324e1b8bb60d0d59d29bcb7c144
Zaranasirova <zerifenesirova784@gmail.com>
2025-01-31 01:54:27 +0400

    Make Project Skills Dynamic

--- src/data.js
+++ src/data.js
@@ -1,5 +1,3 @@
-
-
 export const Contact_data = [
   {
     type: "phone:",
@@ -27,34 +25,47 @@ export const Work_data = [
     description:
       "Это текст. Нажмите один раз и выберите «Редактировать текст» или просто кликните дважды, чтобы добавить свой текст и настроить шрифт. Вы можете переместить его в любое место на странице..",
   },
- 
-  
 ];
 
+export const education_data = [
+  {
+    company: "Unec",
+    position: "Marketing",
+    duration: "2020-2025",
+    description:
+      "Это текст. Нажмите один раз и выберите «Редактировать текст» или просто кликните дважды, чтобы добавить свой текст и настроить шрифт. Вы можете переместить его в любое место на странице..",
+  },
+  {
+    company: "NBEES ACADEMY ",
+    position: "Frontend Developer",
+    duration: "2024",
+    description:
+      "Это текст. Нажмите один раз и выберите «Редактировать текст» или просто кликните дважды, чтобы добавить свой текст и настроить шрифт. Вы можете переместить его в любое место на странице..",
+  },
+  {
+    company: "Texnoera ACADEMY ",
+    position: "Frontend Developer",
+    duration: "2024",
+    description:
+      "Это текст. Нажмите один раз и выберите «Редактировать текст» или просто кликните дважды, чтобы добавить свой текст и настроить шрифт. Вы можете переместить его в любое место на странице..",
+  },
+];
 
-export const education_data=[
-  
-    {
-      company: "Unec",
-      position: "Marketing",
-      duration: "2020-2025",
-      description:
-        "Это текст. Нажмите один раз и выберите «Редактировать текст» или просто кликните дважды, чтобы добавить свой текст и настроить шрифт. Вы можете переместить его в любое место на странице..",
+export const projectsData = [
+  {
+    id: 1,
+    title: "Largest Crypto Marketplace",
+    text: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium sint, illo ab recusandae autem voluptatem.",
+    skills: "React.js,Scss,TypeScript,Redux,react-google-charts,CoinGecko API",
+    github_link:"https://github.com/Zaranasirova/crypto_marketPlace",
+    demo_link:"https://crypto-market-place-pi.vercel.app/",
     },
     {
-      company: "NBEES ACADEMY ",
-      position: "Frontend Developer",
-      duration: "2024",
-      description:
-        "Это текст. Нажмите один раз и выберите «Редактировать текст» или просто кликните дважды, чтобы добавить свой текст и настроить шрифт. Вы можете переместить его в любое место на странице..",
-    },
-    {
-      company: "Texnoera ACADEMY ",
-      position: "Frontend Developer",
-      duration: "2024",
-      description:
-        "Это текст. Нажмите один раз и выберите «Редактировать текст» или просто кликните дважды, чтобы добавить свой текст и настроить шрифт. Вы можете переместить его в любое место на странице..",
-    },
-]
-
-
+      id: 2,
+      title: "Largest Crypto Marketplace",
+      text: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium sint, illo ab recusandae autem voluptatem.",
+      skills: "React.js,Scss,TypeScript,Redux,react-google-charts,CoinGecko API",
+      github_link:"https://github.com/Zaranasirova/crypto_marketPlace",
+      demo_link:"https://crypto-market-place-pi.vercel.app/",
+      },
+];
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,8 +9,15 @@ import Project_1 from "../assets/image/layihe1.png";
 import Project_2 from "../assets/image/layihe2.png";
 import Project_3 from "../assets/image/layihe3.png";
 import Project_4 from "../assets/image/layihe4.png";
+import { projectsData } from "../data";
+
 
 const Projects = () => {
+
+
+
+
+
   return (
     <section className="projects-section">
       <div className="projects-section-title">
@@ -19,51 +26,55 @@ const Projects = () => {
       <div className="projects-wrapper">
         <div className="projects-box-wrapper container">
           <Swiper className="mySwiper">
-            <SwiperSlide className="swiper">
-              <div className="swiper-box">
-                <div className="left-side-swiper">
-                  <div className="project-info">
-                    <h2 className="project-number">01</h2>
-                    <h3 className="project-name">Frontend Projects</h3>
-                    <p className="project-text">
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium sint, illo ab recusandae autem voluptatem.
-                    </p>
-                    <span>Html</span>
+            {
+              projectsData.map((item, index) => (
+                <SwiperSlide className="swiper" key={index}>
+                  <div className="swiper-box">
+                    <div className="left-side-swiper">
+                      <div className="project-info">
+                        <h2 className="project-number">{item.id}</h2>
+                        <h3 className="project-name">{item.title}</h3>
+
+
+                        <p className="project-text">{item.text}</p>
+                        <span>{item.skills}</span>
+
+                      </div>
+                      <div className="project-links">
+                        <Link to={item.demo_link} target="_blank"><GoArrowUpRight /></Link>
+                        <Link to={item.github_link} target="_blank"><FaGithub /></Link>
+                      </div>
+                    </div>
+                    <div className="right-side-swiper">
+                      <div className="project-image">
+                        <Fancybox
+                          options={{
+                            Carousel: {
+                              infinite: true,
+                            },
+                          }}
+                        >
+
+                          <a data-fancybox="gallery" href={Project_1}>
+                            <img src={Project_1} alt="Project 1" />
+                          </a>
+                          <a data-fancybox="gallery" href={Project_2} style={{ display: 'none' }}>
+                            <img src={Project_2} alt="Project 2" />
+                          </a>
+                          <a data-fancybox="gallery" href={Project_3} style={{ display: 'none' }}>
+                            <img src={Project_3} alt="Project 3" />
+                          </a>
+                          <a data-fancybox="gallery" href={Project_4} style={{ display: 'none' }}>
+                            <img src={Project_4} alt="Project 4" />
+                          </a>
+
+                        </Fancybox>
+                      </div>
+                    </div>
                   </div>
-                  <div className="project-links">
-                    <Link to={"/"}><GoArrowUpRight /></Link>
-                    <Link to={"/"}><FaGithub /></Link>
-                  </div>
-                </div>
-                <div className="right-side-swiper">
-                  <div className="project-image">
-                    <Fancybox
-                      options={{
-                        Carousel: {
-                          infinite: true, // Infinite scroll between images
-                        },
-                      }}
-                    >
-                      {/* Initially display only one image */}
-                      <a data-fancybox="gallery"  href={Project_1}>
-                        <img src={Project_1} width="200" height="150" alt="Project 1" />
-                      </a>
-                      {/* All images are part of the same gallery */}
-                      <a data-fancybox="gallery" href={Project_2} style={{ display: 'none' }}>
-                        <img src={Project_2} width="200" height="150" alt="Project 2" />
-                      </a>
-                      <a data-fancybox="gallery" href={Project_3}  style={{ display: 'none' }}>
-                        <img src={Project_3} width="200" height="150" alt="Project 3" />
-                      </a>
-                      <a data-fancybox="gallery" href={Project_4} style={{ display: 'none' }}>
-                        <img src={Project_4} width="200" height="150" alt="Project 4" />
-                      </a>
-      
-                    </Fancybox>
-                  </div>
-                </div>
-              </div>
-            </SwiperSlide>
+                </SwiperSlide>
+              ))
+            }
           </Swiper>
         </div>
       </div>
